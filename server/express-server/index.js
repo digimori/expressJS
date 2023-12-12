bbe435e3f137bbb8964eebb575a843e94aaf2d44
digimori <hanaorikuu@gmail.com>
2023-12-12 19:38:31 +0000

    Get, post, put, patch, delete notes made.

--- server/express-server/index.js
+++ server/express-server/index.js
@@ -7,7 +7,23 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("HELLO!"); 
+  res.send("HELLO!");
+});
+
+app.post("/register", (req, res) => {
+  res.sendStatus(201);
+});
+
+app.put("/user/Cee", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.patch("/user/Cee", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete("/user/Cee", (req, res) => {
+  res.sendStatus(200);
 });
 
 // Listening on the app (port listening for requests from client side, callback which happens when the server runs)
