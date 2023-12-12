271e118a520c03e13e3da8631a93f7d0a15db77e
digimori <hanaorikuu@gmail.com>
2023-12-12 16:15:47 +0000

    HTTP GET request added to index.js

--- server/express-server/index.js
+++ server/express-server/index.js
@@ -6,6 +6,10 @@ const app = express();
 // Specifying port number:
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("HELLO!"); 
+});
+
 // Listening on the app (port listening for requests from client side, callback which happens when the server runs)
 app.listen(port, () => {
   console.log(`Server listening on port ${port}.`);
