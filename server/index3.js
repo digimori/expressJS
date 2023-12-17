f371455fd7fb582c82898768defc9e5e46db4dc1
digimori <hanaorikuu@gmail.com>
2023-12-17 13:21:47 +0000

    Band name generator project completed.

--- server/index3.js
+++ server/index3.js
@@ -3,6 +3,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// For this, we're logging the request method and the URL, then moving to the app.use
+function logger(req, res, next) {
+  console.log("Request method: ", req.method);
+  console.log("Request url: ", req.url);
+  next();
+}
+
+// This will now use the middleware, to run these logs, and then with next() move onto the http requests below it
 app.use(logger);
 
 app.get("/", (req, res) => {
